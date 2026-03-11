444795add35ed98ba9727cd5bcf4a02ca405b4a9
decarvalhojunior-fh <heron@dc.ufc.br>
2026-03-11 08:38:35 -0300

    CG algorithm tested. Tolerance-based termination has been implemented to test with small problem instances.

--- lib/cgx.ex
+++ lib/cgx.ex
@@ -15,26 +15,33 @@ defmodule CGx do
 
   use Application
 
-  def main_loop(_, _, _, 0), do: :ok
-  def main_loop(shift, a, x, it) do
-        {z, rnorm} = cgsolve(a, x)
+  def main_loop(z, _, _, _, rnorm, 0, _), do: {z, Nx.to_number(rnorm)}
+  def main_loop(_, shift, a, x, _, it, tol) do
+        {z, rnorm} = conjgrad(a, x, tol)
         zeta = Nx.add(shift, Nx.divide(1, Nx.dot(x, z)))
         IO.puts("it=#{it}, rnorm=#{rnorm |> Nx.to_number}, zeta=#{zeta |> Nx.to_number} ---")
-        IO.inspect(z)
 
-        x = Nx.divide(z, enorm(z))
-
-        main_loop(shift, a, x, it-1)
+        if Nx.to_number(rnorm) < tol do
+          {z, Nx.to_number(rnorm)}
+        else
+          x = Nx.divide(z, enorm(z))          # update_x
+          main_loop(z, shift, a, x, rnorm, it-1, tol)
+        end
   end
 
-  def cgsolve(a, x) do
+  def conjgrad(a, x, tol) do
 
       {s} = Nx.shape(x)
-      z = Nx.tensor(for _ <- 1..s, do: 0.0)
-      r = x
-      rho = Nx.dot(r, r)
-      p = r
-      z = cgsolve_loop(a, z, r, rho, p, 25)
+
+      z = Nx.broadcast(0.0, {s})            # init_conj_grad
+      r = x                                 # init_conj_grad
+      p = r                                 # init_conj_grad
+
+      rho = Nx.dot(r, r) # ok
+
+      bnorm = enorm(x)
+
+      z = conjgrad_loop(a, z, r, rho, p, 25, tol, bnorm)
 
       rnorm = enorm(Nx.subtract(x, Nx.dot(a,z)))
 
@@ -45,42 +52,160 @@ defmodule CGx do
     Nx.sqrt(Nx.dot(y,y))
   end
 
-  def cgsolve_loop(_, z, _, _, _, 0), do: z
+  def conjgrad_loop(_, z, _, _, _, 0, _, _), do: z
 
-  def cgsolve_loop(a, z, r, rho, p, i) do
+  def conjgrad_loop(a, z, r, rho, p, i, tol, bnorm) do
 
       q = Nx.dot(a, p)
       alpha = Nx.divide(rho, Nx.dot(p, q))
       z = Nx.add(z, Nx.multiply(alpha, p))
-      rho0 = rho
       r = Nx.subtract(r, Nx.multiply(alpha, q))
-      rho = Nx.dot(r, r)
-      beta = Nx.divide(rho, rho0)
 
-      p = Nx.add(r, Nx.multiply(beta, p))
+      rnorm = enorm(r)
 
-      cgsolve_loop(a, z, r, rho, p, i-1)
+      if Nx.to_number(Nx.divide(rnorm, bnorm)) < tol do
+        z
+      else
+        rho0 = rho
+        rho = Nx.dot(r, r)
+        beta = Nx.divide(rho, rho0)
+        p = Nx.add(r, Nx.multiply(beta, p))
+        conjgrad_loop(a, z, r, rho, p, i - 1, tol, bnorm)
+      end
+  end
+
+  def simple_example(niter, tol, shift) do
+
+    a = Nx.tensor([
+            [4.0, 1.0],
+            [1.0, 3.0]
+      ])
+
+    x = Nx.tensor([1.0, 2.0])
+
+    CGx.main_loop(nil, shift, a, x, nil, niter, tol)
+
+  end
+
+  def laplacian_1d_matrix(niter, tol, shift) do
+
+    a = Nx.tensor([
+      [ 2.0, -1.0,  0.0,  0.0],
+      [-1.0,  2.0, -1.0,  0.0],
+      [ 0.0, -1.0,  2.0, -1.0],
+      [ 0.0,  0.0, -1.0,  2.0]
+    ])
+
+    x = Nx.tensor([1.0, 0.0, 0.0, 1.0])
+
+    CGx.main_loop(nil, shift, a, x, nil, niter, tol)
+  end
+
+  def randomic_sparse_matrix(niter, tol, shift) do
+
+    n = 500
+
+    a = generate_spd_sparse(n, 0.05, 10.0)
+    x = generate_rhs(n)
+
+    CGx.main_loop(nil, shift, a, x, nil, niter, tol)
+  end
+
+  def npb_like_matrix(niter, tol, shift) do
+
+    n = 500
+
+    a = generate_npb_like_matrix(n, 10, 10.0)
+    x = generate_rhs(n)
+
+    CGx.main_loop(nil, shift, a, x, nil, niter, tol)
   end
 
   def start(_type, _args) do
+    children = []
 
-    IO.puts("started")
-
-    dim = 4
-    niter = 15
-
-    #a = Nx.tensor(for _ <- 1..dim, do: (for _ <- 1..dim, do: 1.0))   # a is a 2D tensor
-    #x = Nx.tensor(for _ <- 1..dim, do: 1.0)                          # x is a 1D tensors
-
-    a = Nx.tensor([[1, 3, 2, 1], [2, 1, 0, 0], [1, 0, 1, 0], [1, 1, 1, 1]])
-    x = Nx.tensor([-3, 0, 4, -2])
-
-    shift = Nx.tensor(1)                                           # shift is a  scalar
-
-
-    main_loop(shift, a, x, niter)
-
-    IO.puts("finished")
+    opts = [strategy: :one_for_one, name: CGx.Supervisor]
+    Supervisor.start_link(children, opts)
   end
+
+  def main(_args \\ []) do
+
+    niter = 15
+    tol = 1.0e-5
+    shift = Nx.tensor(10)   # shift is a scalar
+
+    {z, rnorm} = randomic_sparse_matrix(niter, tol, shift)
+
+
+    IO.inspect(z, label: "solution")
+    IO.inspect(rnorm, label: "solution residual")
+
+    IO.inspect(rnorm <= tol, label: "residual less than tol?")
+
+    System.halt(0)
+
+  end
+
+
+  def generate_spd_sparse(n, density \\ 0.05, shift \\ 5.0) do
+    key = Nx.Random.key(System.unique_integer())
+
+    {t, key} = Nx.Random.uniform(key, shape: {n, n})
+
+    mask = Nx.less(t, density)
+
+    {t, _} = Nx.Random.normal(key, shape: {n, n})
+
+    values = Nx.multiply(t, mask)
+
+    # tornar simétrica
+    sym =
+      Nx.divide(
+        Nx.add(values, Nx.transpose(values)),
+        2
+      )
+
+    # adicionar shift diagonal
+    Nx.add(sym, Nx.multiply(shift, Nx.eye(n)))
+  end
+
+  def generate_rhs(n) do
+    key = Nx.Random.key(System.unique_integer())
+
+    {t, _} = Nx.Random.normal(key, shape: {n})
+    t
+  end
+
+  def generate_npb_like_matrix(n, nz_per_row \\ 10, shift \\ 10.0) do
+    key = Nx.Random.key(System.unique_integer())
+    rows =
+      for i <- 0..(n-1) do
+        cols =
+          Enum.take_random(0..(n-1), nz_per_row)
+
+        Enum.map(cols, fn j ->
+          {val, _} = Nx.Random.normal(key, shape: {})
+
+          val |> Nx.to_number()
+        end)
+      end
+
+    dense =
+      Nx.tensor(
+        for {row, i} <- Enum.with_index(rows) do
+          r = List.duplicate(0.0, n)
+
+          Enum.reduce(Enum.with_index(row), r, fn {v, j}, acc ->
+            List.replace_at(acc, j, v)
+          end)
+        end
+      )
+
+     # IO.inspect(dense, label: "dense")
+
+    sym = Nx.divide(Nx.add(dense, Nx.transpose(dense)), 2)
+
+    Nx.add(sym, Nx.multiply(shift, Nx.eye(n)))
+ end
 
 end
--- test/cgx_test.exs
+++ test/cgx_test.exs
@@ -2,7 +2,51 @@ defmodule CGxTest do
   use ExUnit.Case
   doctest CGx
 
-  test "greets the world" do
-    assert CGx.hello() == :world
+  test "simple example" do
+
+    niter = 15
+    tol = 1.0e-7
+    shift = Nx.tensor(10)   # shift is a scalar
+
+    {z, rnorm} = CGx.simple_example(niter, tol, shift)
+
+    assert Nx.equal(z, Nx.tensor([0.09090909, 0.6363636]))
+    assert Nx.equal(rnorm, Nx.tensor(0.0))
+  end
+
+  test "Laplacian 1D matrix" do
+
+    niter = 15
+    tol = 1.0e-7
+    shift = Nx.tensor(10)   # shift is a scalar
+
+    {z, rnorm} = CGx.laplacian_1d_matrix(niter, tol, shift)
+
+    assert Nx.equal(z, Nx.tensor([1.0, 1.0, 1.0, 1.0]))
+    assert Nx.equal(rnorm, Nx.tensor(0.0))
+
+  end
+
+  test "Randomic sparse matrix" do
+
+    niter = 15
+    tol = 1.0e-7
+    shift = Nx.tensor(10)   # shift is a scalar
+
+    {z, rnorm} = CGx.randomic_sparse_matrix(niter, tol, shift)
+
+    assert rnorm <= tol
+  end
+
+
+  test "NPB Like matrix (sparse)" do
+
+    niter = 15
+    tol = 1.0e-7
+    shift = Nx.tensor(10)   # shift is a scalar
+
+    {z, rnorm} = CGx.npb_like_matrix(niter, tol, shift)
+
+    assert rnorm <= tol
   end
 end
